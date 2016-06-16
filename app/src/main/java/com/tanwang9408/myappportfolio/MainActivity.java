7b4ceea79c723b15bdf149a205343e33ee8898d6
Tan Wang <timmy940408@me.com>
2016-06-16 23:17:23 +0800

    Finished project 0

--- app/src/main/java/com/tanwang9408/myappportfolio/MainActivity.java
+++ app/src/main/java/com/tanwang9408/myappportfolio/MainActivity.java
@@ -14,10 +14,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+
     public void button1_click(View view) {
         // Do something in response to button click
         Context context = getApplicationContext();
-        CharSequence text = "Hello toast!";
+        CharSequence text = "This button will launch my popular movies app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+
+    public void button2_click(View view) {
+        // Do something in response to button click
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my stock hawk app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+
+    public void button3_click(View view) {
+        // Do something in response to button click
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my build it bigger app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+
+    public void button4_click(View view) {
+        // Do something in response to button click
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my make your app material app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+
+    public void button5_click(View view) {
+        // Do something in response to button click
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my go ubiquitous app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+
+    public void button6_click(View view) {
+        // Do something in response to button click
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my capstone app!";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
