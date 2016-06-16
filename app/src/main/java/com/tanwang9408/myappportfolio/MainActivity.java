decd26915b198f14dd6fb629828a45176ec674fb
Tan Wang <timmy940408@me.com>
2016-06-16 15:53:55 +0800

    Applied styles

--- app/src/main/java/com/tanwang9408/myappportfolio/MainActivity.java
+++ app/src/main/java/com/tanwang9408/myappportfolio/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tanwang9408.myappportfolio;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void button1_click(View view) {
+        // Do something in response to button click
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
     }
 }
